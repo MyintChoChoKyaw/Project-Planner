0f7b739ff1e3477f12a7ad6f2dc22ff0d1f11eb4
MyintChoChoKyaw <myintcho652017@gmail.com>
2024-12-24 16:11:20 +0630

    Adding Css

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="project">
+        <div class="d-flex justify-content-between">
+            <div>
+                <h5 @click="show=!show" class="">{{project.title}}</h5>
+            </div>
+            <div>
+                <i class="fa fa-trash-o p-2 " style="font-size:24px"></i>
+                <i class="fa fa-edit p-2 " style="font-size:24px"></i>
+                <i class="fa fa-check " style="font-size:24px"></i>
+            </div>
+            
+        </div>
+        
+        <p v-if="show">{{project.detail}}</p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    props:['project'],
+    data(){
+        return{
+            show:false
+        }
+    }
+   
+}
+</script>
+
+<style>
+.project{
+    padding:20px;
+    background-color: #f2f2f2;
+    margin:10px;
+    border-radius: 5px;
+    box-shadow: 3px;
+    border-left: 5px solid crimson;
+}
+h5{
+    color:indigo;
+    cursor: pointer;
+}
+i{
+    cursor: pointer;
+}
+i:hover{
+    color:#777;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Projects</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProjectVue :project="project"></SingleProjectVue>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SingleProjectVue from '../components/SingleProject.vue';
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    SingleProjectVue
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((resolve) => {
+        return resolve.json();
+      })
+      .then((data) => {
+        this.projects = data;
+      });
+  },
+};
 </script>
